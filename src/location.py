69a6fbdc70b31c76129952b42edb3887f8e7011b
Adrian <adrian.immel@outlook.de>
2023-06-29 21:51:24 +0200

    implemented percent calculation and json generation

--- src/location.py
+++ src/location.py
@@ -7,14 +7,16 @@ import parser
 class location:
     name: str
     lrz_subdistrict_id: str
+    static_max_clients: int = None
     specific_access_points: Optional[list] = None
-    clients: float = 0
+    clients: int = 0
     capacity_level_in_percent: float = None
     timestamp: int = None
 
     def __post_init__(self):
         parser.get_location_data(self)
+        self.capacity_level_in_percent = parser.percentage_calculator(self)
 
-    def update_clients(self, clients_to_add: float):
+    def update_clients(self, clients_to_add: int):
         if clients_to_add is not None:
-            self.clients = self.clients + clients_to_add
+            self.clients = round(self.clients + clients_to_add)
--- src/parser.py
+++ src/parser.py
@@ -5,8 +5,10 @@ import time
 
 def get_location_data(loc: location):
     url = "http://graphite-kom.srv.lrz.de/render/?from=-1hours&target=ap.ap*-?" + loc.lrz_subdistrict_id + "*.ssid.*&format=json"
-    time.sleep(1)
+    time.sleep(1) # sleep to prevent timeouts from Api
     data_list = requests.get(url, timeout=30).json()
+    if not data_list or None:
+        raise ConnectionError("Could not get or parse the json from LRZ")
     if loc.specific_access_points is not None:
         for ap in data_list:
             for i in loc.specific_access_points:
@@ -14,7 +16,6 @@ def get_location_data(loc: location):
                     datapoint = ap["datapoints"][-2]
                     loc.timestamp = datapoint[1]
                     loc.update_clients(datapoint[0])
-
     else:
         for ap in data_list:
             datapoint = ap["datapoints"][0]
@@ -22,3 +23,9 @@ def get_location_data(loc: location):
             loc.update_clients(datapoint[0])
 
 
+def percentage_calculator(loc: location):
+    percent = loc.clients / loc.static_max_clients
+    percent = round(percent, 2)
+    if percent > 1:
+        percent = 1.0
+    return percent
