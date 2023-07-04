253f271b2b2b0c4cd93b27d0ad786878febc444c
Adrian <immel@hm.edu>
2023-07-05 00:12:54 +0200

    implemented docker support and minor code improvements

--- src/parser.py
+++ src/parser.py
@@ -5,7 +5,7 @@ import time
 
 def get_location_data(loc: location):
     url = "http://graphite-kom.srv.lrz.de/render/?from=-1hours&target=ap.ap*-?" + loc.lrz_subdistrict_id + "*.ssid.*&format=json"
-    time.sleep(1) # sleep to prevent timeouts from Api
+    time.sleep(0.3)  # sleep to prevent ratelimit from Api
     data_list = requests.get(url, timeout=30).json()
     if not data_list or None:
         raise ConnectionError("Could not get or parse the json from LRZ")
