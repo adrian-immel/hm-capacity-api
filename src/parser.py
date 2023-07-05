98a22eae30806d26df3e08dd0ba43e47e0fe8272
Adrian <immel@hm.edu>
2023-07-05 16:46:45 +0200

    added some Documentation in the code

--- src/parser.py
+++ src/parser.py
@@ -2,6 +2,11 @@ import requests
 import location
 import time
 
+"""
+This method is used to get the data from the LRZ API.
+:param loc: the location to get the data from
+"""
+
 
 def get_location_data(loc: location):
     url = "http://graphite-kom.srv.lrz.de/render/?from=-1hours&target=ap.ap*-?" + loc.lrz_subdistrict_id + "*.ssid.*&format=json"
@@ -13,7 +18,7 @@ def get_location_data(loc: location):
         for ap in data_list:
             for i in loc.specific_access_points:
                 if i in ap["target"]:
-                    datapoint = ap["datapoints"][-2]
+                    datapoint = ap["datapoints"][-2]  # get the second last datapoint to prevent inconsistent data
                     loc.timestamp = datapoint[1]
                     loc.update_clients(datapoint[0])
     else:
@@ -21,6 +26,12 @@ def get_location_data(loc: location):
             datapoint = ap["datapoints"][0]
             loc.timestamp = datapoint[1]
             loc.update_clients(datapoint[0])
+
+
+"""
+This method is used to calculate the percentage of the current clients to the maximum clients.
+:param loc: the location object to calculate the percentage of
+"""
 
 
 def percentage_calculator(loc: location):
